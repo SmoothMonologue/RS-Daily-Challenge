3fcbbe97d7cf1164515fecad74b86fd1e52568c1
SmoothMonologue <mobugy99@gmail.com>
2024-03-06 22:41:18 +0900

    할 일을 수정하는 기능을 넣으려다 어려워서 삭제 기능 추가하는 방향으로 우회

--- resat/3rd Day/writing.js
+++ resat/3rd Day/writing.js
@@ -4,20 +4,23 @@ https://hianna.tistory.com/322  콤보 박스
 */
 const upload = document.querySelector(".upload"),
 add = document.querySelector(".add"),
-priority = document.querySelector(".priority")
+priority = document.querySelector(".priority"),
+cancel = document.querySelector(".cancel")
 
 
-let goalList = [], levelList = [];
+let checkable = [], goalList = [], levelList = [];
 let isWriting = false;
 let level;
 
 add.addEventListener("click", initial);
 priority.addEventListener("click", estimate);
 upload.addEventListener("click", addList);
+//cancel.addEventListener("click", switchToList);
 
 function addList() {
     let goal = add.value;
     if (goal != null) {
+        checkable.push(false);
         goalList.push(goal);
         levelList.push(level);
         isWriting = false;
@@ -30,12 +33,59 @@ function addList() {
 
 function showList() {
     let list = "<ul>"
-    for (let i = 0; i <goalList.length; i++) {
-        list += "<input type='checkbox' name='accomplish'/>" + goalList[i] + "<input type='button' value=" + levelList[i] + "><input type='button' class='modify' value='수정'><br>";
+    for (let i = 0; i < goalList.length; i++) {
+        if (checkable[i]) {
+            list += "<input type='checkbox' class='accomplish' id=" + i + " checked>" + goalList[i] + "<button id='level'>" + levelList[i] + "</button><input type='button' class='modify' id=" + i + " value='삭제'><br>";
+        }
+        else {
+            list += "<input type='checkbox' class='accomplish' id=" + i + ">" + goalList[i] + "<button id='level'>" + levelList[i] + "</button><input type='button' class='modify' id=" + i + " value='삭제'><br>";
+        }
     }
     list += "</ul>";
     document.querySelector(".fullList").innerHTML = list;
+
+    let checkButtons = document.querySelectorAll(".accomplish");
+    let modifyButtons = document.querySelectorAll(".modify");
+    for (let i = 0; i < modifyButtons.length; i++) {
+        checkButtons[i].addEventListener("click", checking);
+        modifyButtons[i].addEventListener("click", deleteList);
+    }
 }
+
+function checking() {
+    let id = this.getAttribute("id");
+    if (checkable[id]) {
+        checkable[id] = false;
+    }
+    else {
+        checkable[id] = true;
+    }
+}
+
+function deleteList() {
+    let id = this.getAttribute("id");
+    checkable.splice(id, 1);
+    goalList.splice(id, 1);
+    levelList.splice(id, 1);
+    showList();
+}
+
+/*function switchToEdit() {
+    console.log(location.search);
+    location = "./modify.html";
+    console.log(location.search);
+
+}
+
+function switchToList() {
+    location = "./list.html";
+}
+
+function editList() {
+    let id = this.getAttribute("id");
+    console.log(id);
+
+}*/
 
 function initial() {
     if (!isWriting) {
