854bf082b4bf59b7c8e1b2c6dcf0678f776bc1d8
SmoothMonologue <mobugy99@gmail.com>
2024-03-06 18:27:20 +0900

    우선 순위가 다른 복수의 작업을 등록할 때 우선순위가 제대로 적용 안 되던 현상 개선

--- resat/3rd Day/writing.js
+++ resat/3rd Day/writing.js
@@ -1,20 +1,29 @@
-
+/*참고 문헌
+https://velog.io/@janeljs/making-a-todolist 리스트 작성
+https://hianna.tistory.com/322  콤보 박스
+*/
 const upload = document.querySelector(".upload"),
-add = document.querySelector(".add")
+add = document.querySelector(".add"),
+priority = document.querySelector(".priority")
 
 
-let goalList = [];
+let goalList = [], levelList = [];
 let isWriting = false;
+let level;
 
 add.addEventListener("click", initial);
+priority.addEventListener("click", estimate);
 upload.addEventListener("click", addList);
 
 function addList() {
     let goal = add.value;
     if (goal != null) {
         goalList.push(goal);
+        levelList.push(level);
         isWriting = false;
         initial();
+        priority.value = "none";
+        upload.disabled = true;
     }
     showList();
 }
@@ -22,7 +31,7 @@ function addList() {
 function showList() {
     let list = "<ul>"
     for (let i = 0; i <goalList.length; i++) {
-        list += "<input type='checkbox' name='accomplish'/>" + goalList[i] + "<span class='close' id=" + i + ">" + "\u00D7" + "</span><br>";
+        list += "<input type='checkbox' name='accomplish'/>" + goalList[i] + "<input type='button' value=" + levelList[i] + "><input type='button' class='modify' value='수정'><br>";
     }
     list += "</ul>";
     document.querySelector(".fullList").innerHTML = list;
@@ -31,6 +40,14 @@ function showList() {
 function initial() {
     if (!isWriting) {
         add.value = "";
+        add.focus();
     }
     isWriting = true;
+}
+
+function estimate() {
+    level = priority.value;
+    if (level != "none") {
+        upload.disabled=false;
+    }
 }
