7737707a877b28a3ae94d31874c5ca51ccda53ce
SmoothMonologue <mobugy99@gmail.com>
2024-03-07 17:57:05 +0900

    오늘 날짜 표시 + 달 이동

--- resat/4th Day/timeRendering.js
+++ resat/4th Day/timeRendering.js
@@ -2,21 +2,32 @@
 https://songsong.dev/entry/Javascript로-달력-만들기
 */
 const calendar = document.querySelector(".dates"),
-title = document.querySelector(".year-month")
+title = document.querySelector(".year-month"),
+prev = document.querySelector(".go-prev"),
+next = document.querySelector(".go-next")
 
 var date = new Date(); 
 var utc = date.getTime() + (date.getTimezoneOffset() * 60 * 1000); 
 var kstGap = 9 * 60 * 60 * 1000; 
 var today = new Date(utc + kstGap);
 var thisMonth = new Date(today.getFullYear(), today.getMonth(), today.getDate());
+var currentYear = thisMonth.getFullYear(); 
+var currentMonth = thisMonth.getMonth();
 
 renderCalendar(thisMonth);
 
-function renderCalendar(thisMonth) {
-    var currentYear = thisMonth.getFullYear(); 
-    var currentMonth = thisMonth.getMonth();
-    var todayDate;
+prev.addEventListener("click", function() { 
+    thisMonth = new Date(currentYear, currentMonth - 1, 1); renderCalendar(thisMonth);
+});
+next.addEventListener("click", function() { 
+    thisMonth = new Date(currentYear, currentMonth + 1, 1); renderCalendar(thisMonth);
+});
 
+function renderCalendar(selectedMonth) {
+    currentYear = selectedMonth.getFullYear(); 
+    currentMonth = selectedMonth.getMonth();
+
+    var todayDate;
     var startDay = new Date(currentYear, currentMonth, 0);
     var prevDate = startDay.getDate();
     var prevDay = startDay.getDay();
@@ -31,11 +42,9 @@ function renderCalendar(thisMonth) {
     for (var i = prevDate - prevDay + 1; i <= prevDate; i++) {
         calendar.innerHTML += '<div class="day prev disable">' + i + '</div>'
     }
-
     for (var i = 1; i <= nextDate; i++) {
         calendar.innerHTML += '<div class="day current">' + i + '</div>'
     }
-
     for (var i = 1; i <= (7 - nextDay == 7 ? 0 : 7 - nextDay); i++) {
         calendar.innerHTML += '<div class="day next disable">' + i + '</div>'
     }
