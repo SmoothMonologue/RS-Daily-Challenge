f0ab8a0ebe2971dbb7bee5fc02e12e47af672a9a
SmoothMonologue <mobugy99@gmail.com>
2024-03-07 21:55:47 +0900

    메모 저장 및 수정 기능 구현

--- resat/4th Day/timeRendering.js
+++ resat/4th Day/timeRendering.js
@@ -6,7 +6,8 @@ title = document.querySelector(".year-month"),
 prev = document.querySelector(".go-prev"),
 next = document.querySelector(".go-next"),
 viewMemo = document.getElementById("memo"),
-selected = document.querySelector(".selected")
+selected = document.querySelector(".selected"),
+savedContent = document.getElementById("content")
 
 var date = new Date(); 
 var utc = date.getTime() + (date.getTimezoneOffset() * 60 * 1000); 
@@ -15,7 +16,8 @@ var today = new Date(utc + kstGap);
 var thisMonth = new Date(today.getFullYear(), today.getMonth(), today.getDate());
 var currentYear = thisMonth.getFullYear(); 
 var currentMonth = thisMonth.getMonth();
-var allMemo = [][2];
+var dateOfMemo = [], contentOfMemo = [];
+var isNull = true;
 
 renderCalendar(thisMonth);
 
@@ -62,18 +64,42 @@ function renderCalendar(selectedMonth) {
 function showMemo(i) {
     viewMemo.style.visibility = 'visible';
     selected.innerHTML = currentYear + '.' + (currentMonth + 1) + '.' + i;
-    console.log(viewMemo.style.visibility);
+    uploadMemo();
+    //console.log(viewMemo.style.visibility);
 }
 
 function hideMemo() {
     viewMemo.style.visibility = 'hidden';
-    console.log(viewMemo.style.visibility);
+    //console.log(viewMemo.style.visibility);
 }
 
 function saveMemo() {
-
+    isNull = true;
+    for (var i = 0; i < dateOfMemo.length; i++) {
+        if (selected.innerHTML == dateOfMemo[i]) {
+            contentOfMemo[i] = savedContent.value;
+            isNull = false;
+            console.log(dateOfMemo, contentOfMemo);
+            break;
+        }
+    }
+    if (isNull) {
+        dateOfMemo.push(selected.innerHTML);
+        contentOfMemo.push(savedContent.value);
+        console.log(dateOfMemo, contentOfMemo);
+    }
 }
 
 function uploadMemo() {
-
+    isNull = true;
+    for (var i = 0; i < dateOfMemo.length; i++) {
+        if (selected.innerHTML == dateOfMemo[i]) {
+            savedContent.value = contentOfMemo[i];
+            isNull = false;
+            break;
+        }
+    }
+    if (isNull) {
+        savedContent.value = "메모를 입력할 수 있습니다.";
+    }
 }
