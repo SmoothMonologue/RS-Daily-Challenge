940844aa90d751ffbd640472de94b7983c049e5a
SmoothMonologue <mobugy99@gmail.com>
2024-03-11 13:31:46 +0900

    슬라이드 버튼 동작 확인

---
+++ resat/6-8th Day/slide.js
@@ -0,0 +1,23 @@
+/* 참고 문헌
+https://velog.io/@grinding_hannah/JavaScript-자바스크립트로-캐러셀Carousel-구현하기
+*/
+
+const container = document.querySelector(".container"),
+prevBtn = document.querySelector(".prev"),
+nextBtn = document.querySelector(".next")
+
+prevBtn.addEventListener('click', translateContainer.bind(this, 1));
+nextBtn.addEventListener('click', translateContainer.bind(this, -1));
+
+function translateContainer(direction) {
+    const selectedBtn = (direction === 1) ? 'prev' : 'next';
+
+    container.style.transitionDuration = '500ms';
+    container.style.transform = `translateX(${direction * (100 / 2.5)}%)`;
+    container.ontransitionend = () => reorganizeEl(selectedBtn);
+}
+
+function reorganizeEl(selectedBtn) {
+    container.removeAttribute('style');
+    (selectedBtn === 'prev') ? container.insertBefore(container.lastElementChild, container.firstElementChild): container.appendChild(container.firstElementChild);
+  }
